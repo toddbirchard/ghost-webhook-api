323a0258ec7dbeb01e39fc1ca4b3b50faccc828e
Todd Birchard <toddbirchard@gmail.com>
2020-03-31 21:02:49 -0400

    Linter errors.

--- api/algolia/routes.py
+++ api/algolia/routes.py
@@ -7,7 +7,7 @@ from .fetch import fetch_weekly_searches
 @api.route('/searches/week', methods=['GET'])
 def week_searches():
     """Save top Algolia searches for the current week."""
-    records = weekly_searches()
+    records = fetch_weekly_searches()
     upload = db.insert_records(records, 'algolia_searches_week', replace=True)
     return make_response(jsonify(upload), 200)
 
@@ -15,6 +15,6 @@ def week_searches():
 @api.route('/searches/historical', methods=['GET'])
 def historical_searches():
     """Horde historical Algolia searches."""
-    records = weekly_searches()
+    records = fetch_weekly_searches()
     upload = db.insert_records(records, 'algolia_searches_historical')
     return make_response(jsonify(upload), 200)
