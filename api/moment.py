17ae532f4b42b38449da449c9d54c0262039ac91
Todd Birchard <toddbirchard@gmail.com>
2020-07-14 01:11:05 -0400

    Timezone error.

--- api/moment.py
+++ api/moment.py
@@ -6,5 +6,5 @@ from datetime import datetime, timedelta
 def get_current_time(updated_at):
 	"""Get current EST time"""
 	updated_time = updated_at.replace('z', '')
-	updated_time = datetime.strptime(updated_time, "%Y-%m-%dT%H:%M:%S.%LZ") + timedelta(seconds=1)
-	return updated_time.strftime("%Y-%m-%dT%H:%M:%S.%LZ").replace(' ', '')
+	updated_time = datetime.strptime(updated_time, "%Y-%m-%dT%H:%M:%S.%fZ") + timedelta(seconds=1)
+	return updated_time.strftime("%Y-%m-%dT%H:%M:%S.%fZ").replace(' ', '')
