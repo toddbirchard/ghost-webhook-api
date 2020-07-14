23f1c8123d3f54c8d1073021818e300b136bc643
Todd Birchard <toddbirchard@gmail.com>
2020-07-14 01:34:05 -0400

    Timezone error.

--- api/posts/lynx/cards.py
+++ api/posts/lynx/cards.py
@@ -11,11 +11,7 @@ def format_lynx_posts(post):
     """Replace <a> tags in Lynx posts with link previews."""
     html = post.get('html')
     links = re.findall('<a href="(.*?)"', html)
-    link_previews = [scrape_link(link) for link in links]
-    try:
-        link_previews.remove(None)
-    except ValueError:
-        pass
+    link_previews = filter(None, [scrape_link(link) for link in links])
     mobile_doc['cards'] = link_previews
     for i, link in enumerate(link_previews):
         mobile_doc['sections'].append([10, i])
