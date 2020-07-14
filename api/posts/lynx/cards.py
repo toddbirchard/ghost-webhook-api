c3608d7fe16cd7ac4757e9f50908c66b6699d784
Todd Birchard <toddbirchard@gmail.com>
2020-07-13 23:40:45 -0400

    Deal with extruct as list data.

--- api/posts/lynx/cards.py
+++ api/posts/lynx/cards.py
@@ -11,7 +11,11 @@ def format_lynx_posts(post):
     """Replace <a> tags in Lynx posts with link previews."""
     html = post.get('html')
     links = re.findall('<a href="(.*?)"', html)
-    link_previews = [scrape_link(link) for link in links if scrape_link(link) is not None]
+    link_previews = [scrape_link(link) for link in links]
+    try:
+        link_previews.remove(None)
+    except ValueError:
+        pass
     mobile_doc['cards'] = link_previews
     for i, link in enumerate(link_previews):
         mobile_doc['sections'].append([10, i])
