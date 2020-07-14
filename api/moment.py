41e9a7be542f31344086d37e81daaef4d56f4b02
Todd Birchard <toddbirchard@gmail.com>
2020-07-14 01:49:58 -0400

    Timezone error.

--- api/moment.py
+++ api/moment.py
@@ -8,4 +8,4 @@ def get_current_time(updated_at):
 	# updated_time = updated_at.replace('z', '')
 	# updated_time = datetime.strptime(updated_time, "%Y-%m-%dT%H:%M:%S.%fZ") + timedelta(seconds=1, minutes=1)
 	now = datetime.now(pytz.utc)
-	return now.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + 'Z'
+	return now.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-6] + '000Z'
