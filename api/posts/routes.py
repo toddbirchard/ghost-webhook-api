006002a16aa112c6183aaaa34aeaaa2effec7c55
Todd Birchard <toddbirchard@gmail.com>
2020-08-03 22:47:47 -0400

    Update mobiledoc via db query.

--- api/posts/routes.py
+++ api/posts/routes.py
@@ -45,7 +45,7 @@ def set_post_metadata():
         # Parse link previews
         if 'kg-card' not in mobiledoc:
             doc = generate_link_previews(post)
-            db.execute_query(f"UPDATE posts SET mobiledoc = '{doc}' WHERE id = {id};")
+            db.execute_query(f"UPDATE posts SET mobiledoc = '{doc}' WHERE id = '{id}';")
             LOGGER.info()
     # Update image meta tags
     elif feature_image is not None:
