d0eb146ac9d5ca46640e37f41b9e42349e0ad4b4
Todd Birchard <toddbirchard@gmail.com>
2020-08-09 19:25:25 -0400

    Github endpoint.

--- api/github/__init__.py
+++ api/github/__init__.py
@@ -6,17 +6,34 @@ from api.log import LOGGER
 
 
 @LOGGER.catch
-@api.route('/github/notifications', methods=['POST'])
-def github_notification():
-    """Send SMS notification upon Github activity by users."""
+@api.route('/github/pr', methods=['POST'])
+def github_pr_opened():
+    """Send SMS notification for all PR activity."""
     payload = request.get_json()
     action = payload.get('action')
     user = payload['sender'].get('login')
+    pull_request = payload['pull_request']
+    repo = payload['repository']
     headers = {'content-type': 'text/html; charset=UTF-8'}
     if user == 'toddbirchard':
         return make_response('Activity ignored.', 200, headers)
-    sms.send_message(payload)
+    message = f'PR {action} for repository {repo["name"]}: `{pull_request["title"]}` \n\n {pull_request["url"]}'
+    sms.send_message(message)
     return make_response(f'SMS notification sent for {action} for {user}.', 200, headers)
 
 
-
+@LOGGER.catch
+@api.route('/github/issue', methods=['POST'])
+def github_pr_opened():
+    """Send SMS notification upon issue creation."""
+    payload = request.get_json()
+    action = payload.get('action')
+    user = payload['sender'].get('login')
+    issue = payload['issue']
+    repo = payload['repository']
+    headers = {'content-type': 'text/html; charset=UTF-8'}
+    if user == 'toddbirchard':
+        return make_response('Activity ignored.', 200, headers)
+    message = f'Issue {action} for repository {repo["name"]}: `{issue["title"]}` \n\n {issue["url"]}'
+    sms.send_message(message)
+    return make_response(f'SMS notification sent for {action} for {user}.', 200, headers)
