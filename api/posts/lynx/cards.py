a3b4f407f8d1c253c17b3e29b85a7dc19a4a5e17
Todd Birchard <toddbirchard@gmail.com>
2020-08-09 16:28:16 -0400

    Rewrite `scrape.py`, remove obsolete metadata library.

--- api/posts/lynx/cards.py
+++ api/posts/lynx/cards.py
@@ -15,4 +15,4 @@ def generate_link_previews(post) -> str:
     mobile_doc['cards'] = link_previews
     for i, link in enumerate(link_previews):
         mobile_doc['sections'].append([10, i])
-    return json.dumps(mobile_doc)
+    return json.dumps(mobile_doc).replace('\\', '\\\\')
