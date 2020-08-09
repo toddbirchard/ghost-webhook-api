6555e680f8b64a34ef3cb558f32c992010cfe290
Todd Birchard <toddbirchard@gmail.com>
2020-08-09 19:12:12 -0400

    Github endpoint.

--- api/github/__init__.py
+++ api/github/__init__.py
@@ -10,12 +10,12 @@ from api.log import LOGGER
 def update_post():
     """Send SMS notification upon Github activity by users."""
     payload = request.get_json()
-    action = payload['action']
+    action = payload.get('action')
     user = payload['sender'].get('login')
     if user == 'toddbirchard':
         return make_response('Activity ignored.', 200, {'content-type': 'text/html; charset=UTF-8'})
     sms.send_message(payload)
-    return make_response('SMS notification sent.', 200, {'content-type': 'text/html; charset=UTF-8'})
+    return make_response(f'SMS notification sent for {action} for {user}.', 200, {'content-type': 'text/html; charset=UTF-8'})
 
 
 
