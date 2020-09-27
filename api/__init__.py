76f99f55960a6a8b5607c90e04457f96d5112223
Todd Birchard <toddbirchard@gmail.com>
2020-09-27 02:52:37 -0400

    CORS headers.

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,6 @@
 """Initialize api."""
 from flask import Flask
+from flask_cors import CORS
 from ddtrace import patch_all
 patch_all()
 
@@ -8,6 +9,7 @@ def init_api():
     """Construct the core application."""
     api = Flask(__name__, instance_relative_config=False)
     api.config.from_object('config.Config')
+    CORS(api, resources={r"/*": {"origins": "*"}})
     with api.app_context():
         from api import (
             posts,
