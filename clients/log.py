d304f8d2ebe9840207ab1d12176121ef41567c9d
Todd Birchard <toddbirchard@gmail.com>
2020-10-15 17:25:09 -0400

    Add level to logs.

--- clients/log.py
+++ clients/log.py
@@ -1,25 +1,26 @@
 """Custom logger."""
-from sys import stdout, stderr
+from sys import stdout
 import simplejson as json
 from loguru import logger
 from config import Config
 
 
-def serialize(record):
-    """Parse log message into Datadog JSON format."""
-    subset = {
-        "time": record["time"].strftime("%m/%d/%Y, %H:%M:%S"),
-        "message": record["message"],
-        "function": record.get("function"),
-        "module": record.get("name")
-    }
-    if record.get("exception", None):
-        subset.update({'exception': record["exception"]})
-    return json.dumps(subset)
-
-
 def formatter(record):
     """Pass raw string to be serialized."""
+
+    def serialize(log):
+        """Parse log message into Datadog JSON format."""
+        subset = {
+            "time": log["time"].strftime("%m/%d/%Y, %H:%M:%S"),
+            "message": log["message"],
+            "level": log["level"].name,
+            "function": log.get("function"),
+            "module": log.get("name")
+        }
+        if log.get("exception", None):
+            subset.update({'exception': log["exception"]})
+        return json.dumps(subset)
+
     record["extra"]["serialized"] = serialize(record)
     return "{extra[serialized]},\n"
 
@@ -30,28 +31,9 @@ def create_logger() -> logger:
     logger.add(
         stdout,
         colorize=True,
-        level="INFO",
         catch=True,
         format="<light-cyan>{time:MM-DD-YYYY HH:mm:ss}</light-cyan> | "
                + "<light-green>{level}</light-green>: "
-               + "<light-white>{message}</light-white>"
-    )
-    logger.add(
-        stdout,
-        colorize=True,
-        level="WARNING",
-        catch=True,
-        format="<light-cyan>{time:MM-DD-YYYY HH:mm:ss}</light-cyan> | "
-               + "<yellow>{level}</yellow>: "
-               + "<light-white>{message}</light-white>"
-    )
-    logger.add(
-        stderr,
-        colorize=True,
-        level="ERROR",
-        catch=True,
-        format="<light-cyan>{time:MM-DD-YYYY HH:mm:ss}</light-cyan> | "
-               + "<red>{level}</red>: "
                + "<light-white>{message}</light-white>"
     )
     # Datadog
@@ -59,6 +41,7 @@ def create_logger() -> logger:
         'logs/info.json',
         format=formatter,
         rotation="500 MB",
+        compression="zip"
     )
     if Config.FLASK_ENV == 'production':
         # APM
