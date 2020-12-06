7cd82dda8a4cb10649e51c992ee6b0b926c7d946
Todd Birchard <toddbirchard@gmail.com>
2020-12-06 14:49:44 -0500

    ASGI config.

--- asgi.py
+++ asgi.py
@@ -6,4 +6,4 @@ from app import init_api
 api = init_api()
 
 if __name__ == "__main__":
-    uvicorn.run(api, host="0.0.0.0", port=5000)
+    uvicorn.run('asgi:api', host="0.0.0.0", port=8000, workers=4)
