df53dd9c6c1b2a3143bba4e0a9b5befb043efee3
Todd Birchard <toddbirchard@gmail.com>
2020-12-06 17:52:34 -0500

    Uvicorn.

--- asgi.py
+++ asgi.py
@@ -1,9 +1,5 @@
 """Application entry point."""
 import uvicorn
 
-from app import init_api
-
-api = init_api()
-
 if __name__ == "__main__":
-    uvicorn.run('asgi:api', host="0.0.0.0", port=8001, workers=4)
+    uvicorn.run('app:api', host="0.0.0.0", workers=4)
