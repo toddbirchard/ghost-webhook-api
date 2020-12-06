e027ade4cec7467fba0f8f1730772fc9275adfc8
Todd Birchard <toddbirchard@gmail.com>
2020-12-06 14:24:57 -0500

    ASGI app factory.

--- app/__init__.py
+++ app/__init__.py
@@ -15,37 +15,38 @@ def get_settings():
     return Settings()
 
 
-Base.metadata.create_all(bind=engine)
+def init_api():
+    Base.metadata.create_all(bind=engine)
 
+    api = FastAPI(
+        title="Jamstack API",
+        description="API to automate optimizations for JAMStack sites.",
+        version="0.1.0",
+        debug=True,
+        docs_url="/",
+        openapi_url="/api.json",
+        openapi_tags=Settings().API_TAGS,
+    )
 
-api = FastAPI(
-    title="Jamstack API",
-    description="API to automate optimizations for JAMStack sites.",
-    version="0.1.0",
-    debug=True,
-    docs_url="/",
-    openapi_url="/api.json",
-    openapi_tags=Settings().API_TAGS,
-)
+    settings = Depends(get_settings)
 
-settings = Depends(get_settings)
+    api.add_middleware(
+        CORSMiddleware,
+        allow_origins=Settings().CORS_ORIGINS,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
-api.add_middleware(
-    CORSMiddleware,
-    allow_origins=Settings().CORS_ORIGINS,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+    api.include_router(analytics.router)
+    api.include_router(newsletter.router)
+    api.include_router(posts.router)
+    api.include_router(accounts.router)
+    api.include_router(authors.router)
+    api.include_router(images.router)
+    api.include_router(github.router)
 
-api.include_router(analytics.router)
-api.include_router(newsletter.router)
-api.include_router(posts.router)
-api.include_router(accounts.router)
-api.include_router(authors.router)
-api.include_router(images.router)
-api.include_router(github.router)
+    if Settings().ENVIRONMENT == "production":
+        api = TraceMiddleware(api)
 
-
-if Settings().ENVIRONMENT == "production":
-    api = TraceMiddleware(api)
+    return api
--- asgi.py
+++ asgi.py
@@ -1,5 +1,9 @@
 """Application entry point."""
 import uvicorn
 
+from app import init_api
+
+api = init_api()
+
 if __name__ == "__main__":
-    uvicorn.run("app:api", host="0.0.0.0", port=5000)
+    uvicorn.run(api, host="0.0.0.0", port=5000)
