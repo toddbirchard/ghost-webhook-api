a5bf1e6cda428fbc77b86048034c08d75d3321d9
Todd Birchard <toddbirchard@gmail.com>
2020-12-06 00:29:29 -0500

    Config type checking.

--- config.py
+++ config.py
@@ -18,10 +18,10 @@ class Settings(BaseSettings):
     debug: bool = True
 
     # General Config
-    SECRET_KEY = getenv("SECRET_KEY")
+    SECRET_KEY: str = getenv("SECRET_KEY")
     ENVIRONMENT: str = getenv("ENVIRONMENT")
     dt: datetime.datetime = datetime.datetime.today()
-    CORS_ORIGINS = [
+    CORS_ORIGINS: list = [
         "http://hackersandslackers.com",
         "https://hackersandslackers.app",
         "http://localhost",
@@ -63,57 +63,57 @@ class Settings(BaseSettings):
     env_file = ".env"
 
     # Database
-    SQLALCHEMY_DATABASE_URI = getenv("SQLALCHEMY_DATABASE_URI")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ENGINE_OPTIONS = {
+    SQLALCHEMY_DATABASE_URI: str = getenv("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_TRACK_MODIFICATIONS: bool = False
+    SQLALCHEMY_ENGINE_OPTIONS: dict = {
         "ssl": {"ca": "/Users/toddbirchard/auth/ca-certificate.crt"}
     }
 
     # Algolia API
-    ALGOLIA_BASE_URL = getenv("ALGOLIA_BASE_URL")
-    ALGOLIA_APP_ID = getenv("ALGOLIA_APP_ID")
-    ALGOLIA_API_KEY = getenv("ALGOLIA_API_KEY")
+    ALGOLIA_BASE_URL: str = getenv("ALGOLIA_BASE_URL")
+    ALGOLIA_APP_ID: str = getenv("ALGOLIA_APP_ID")
+    ALGOLIA_API_KEY: str = getenv("ALGOLIA_API_KEY")
 
     # Google Cloud storage
-    GCP_BUCKET_URL = getenv("GCP_BUCKET_URL")
-    GCP_BUCKET_NAME = getenv("GCP_BUCKET_NAME")
-    GOOGLE_APPLICATION_CREDENTIALS = getenv("GOOGLE_APPLICATION_CREDENTIALS")
-    GCP_BUCKET_FOLDER = [f'{dt.year}/{dt.strftime("%m")}']
-    GCP_LYNX_DIRECTORY = "roundup"
+    GCP_BUCKET_URL: str = getenv("GCP_BUCKET_URL")
+    GCP_BUCKET_NAME: str = getenv("GCP_BUCKET_NAME")
+    GOOGLE_APPLICATION_CREDENTIALS: str = getenv("GOOGLE_APPLICATION_CREDENTIALS")
+    GCP_BUCKET_FOLDER: list = [f'{dt.year}/{dt.strftime("%m")}']
+    GCP_LYNX_DIRECTORY: str = "roundup"
 
     # Google BigQuery
-    GCP_PROJECT = getenv("GCP_PROJECT")
-    GCP_BIGQUERY_TABLE = getenv("GCP_BIGQUERY_TABLE")
-    GCP_BIGQUERY_DATASET = getenv("GCP_BIGQUERY_DATASET")
-    GCP_BIGQUERY_URI = f"bigquery://{GCP_PROJECT}/{GCP_BIGQUERY_DATASET}"
+    GCP_PROJECT: str = getenv("GCP_PROJECT")
+    GCP_BIGQUERY_TABLE: str = getenv("GCP_BIGQUERY_TABLE")
+    GCP_BIGQUERY_DATASET: str = getenv("GCP_BIGQUERY_DATASET")
+    GCP_BIGQUERY_URI: str = f"bigquery://{GCP_PROJECT}/{GCP_BIGQUERY_DATASET}"
 
     # Ghost
-    GHOST_BASE_URL = getenv("GHOST_BASE_URL")
-    GHOST_ADMIN_API_URL = f"{GHOST_BASE_URL}/ghost/api/v3/admin"
-    GHOST_CONTENT_API_URL = f"{GHOST_BASE_URL}/ghost/api/v3/"
-    GHOST_API_USERNAME = getenv("GHOST_API_USERNAME")
-    GHOST_API_PASSWORD = getenv("GHOST_API_PASSWORD")
-    GHOST_CLIENT_ID = getenv("GHOST_CLIENT_ID")
-    GHOST_ADMIN_API_KEY = getenv("GHOST_ADMIN_API_KEY")
-    GHOST_API_EXPORT_URL = f"{GHOST_BASE_URL}/admin/db/"
-    GHOST_NETLIFY_BUILD_HOOK = getenv("GHOST_NETLIFY_BUILD_HOOK")
+    GHOST_BASE_URL: str = getenv("GHOST_BASE_URL")
+    GHOST_ADMIN_API_URL: str = f"{GHOST_BASE_URL}/ghost/api/v3/admin"
+    GHOST_CONTENT_API_URL: str = f"{GHOST_BASE_URL}/ghost/api/v3/"
+    GHOST_API_USERNAME: str = getenv("GHOST_API_USERNAME")
+    GHOST_API_PASSWORD: str = getenv("GHOST_API_PASSWORD")
+    GHOST_CLIENT_ID: str = getenv("GHOST_CLIENT_ID")
+    GHOST_ADMIN_API_KEY: str = getenv("GHOST_ADMIN_API_KEY")
+    GHOST_API_EXPORT_URL: str = f"{GHOST_BASE_URL}/admin/db/"
+    GHOST_NETLIFY_BUILD_HOOK: str = getenv("GHOST_NETLIFY_BUILD_HOOK")
 
     # Mailgun
-    MAILGUN_EMAIL_SERVER = getenv("MAILGUN_EMAIL_SERVER")
-    MAILGUN_EMAIL_TEMPLATE = getenv("MAILGUN_EMAIL_TEMPLATE")
-    MAILGUN_API_KEY = getenv("MAILGUN_API_KEY")
-    MAILGUN_FROM_SENDER = getenv("MAILGUN_FROM_SENDER")
-    MAILGUN_SUBJECT_LINE = "To Hack or to Slack; That is the Question."
-    MAILGUN_PERSONAL_EMAIL = getenv("MAILGUN_PERSONAL_EMAIL")
+    MAILGUN_EMAIL_SERVER: str = getenv("MAILGUN_EMAIL_SERVER")
+    MAILGUN_EMAIL_TEMPLATE: str = getenv("MAILGUN_EMAIL_TEMPLATE")
+    MAILGUN_API_KEY: str = getenv("MAILGUN_API_KEY")
+    MAILGUN_FROM_SENDER: str = getenv("MAILGUN_FROM_SENDER")
+    MAILGUN_SUBJECT_LINE: str = "To Hack or to Slack; That is the Question."
+    MAILGUN_PERSONAL_EMAIL: str = getenv("MAILGUN_PERSONAL_EMAIL")
 
     # Mixpanel
-    MIXPANEL_API_TOKEN = getenv("MIXPANEL_API_TOKEN")
+    MIXPANEL_API_TOKEN: str = getenv("MIXPANEL_API_TOKEN")
 
     # Twilio
-    TWILIO_SENDER_PHONE = getenv("TWILIO_SENDER_PHONE")
-    TWILIO_RECIPIENT_PHONE = getenv("TWILIO_RECIPIENT_PHONE")
-    TWILIO_AUTH_TOKEN = getenv("TWILIO_AUTH_TOKEN")
-    TWILIO_ACCOUNT_SID = getenv("TWILIO_ACCOUNT_SID")
+    TWILIO_SENDER_PHONE: str = getenv("TWILIO_SENDER_PHONE")
+    TWILIO_RECIPIENT_PHONE: str = getenv("TWILIO_RECIPIENT_PHONE")
+    TWILIO_AUTH_TOKEN: str = getenv("TWILIO_AUTH_TOKEN")
+    TWILIO_ACCOUNT_SID: str = getenv("TWILIO_ACCOUNT_SID")
 
     # Datadog
     DATADOG_API_KEY: str = getenv("DATADOG_API_KEY")
