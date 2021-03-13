7cab992a5c4f32ca652097d42bbf9149db2b57f5
Todd <toddbirchard@gmail.com>
2021-03-13 13:15:20 -0500

    COnsolidate author endpoints into one.

--- app/authors/__init__.py
+++ app/authors/__init__.py
@@ -9,32 +9,10 @@ from database.schemas import PostUpdate
 router = APIRouter(prefix="/authors", tags=["authors"])
 
 
-@router.post("/post/created")
+@router.post("/post")
 async def author_post_created(post_update: PostUpdate):
     """
-    Notify admin when new authors create their first post.
-
-    :param post_update: Post object generated upon update.
-    :param post_update: PostUpdate
-    """
-    data = post_update.post.current
-    title = data.title
-    author_name = data.primary_author.name
-    primary_author_id = data.primary_author.id
-    if primary_author_id not in ("1", "5dc42cb612c9ce0d63f5bf39"):
-        msg = f"{author_name} just created a post: `{title}`."
-        LOGGER.info(f"SMS notification triggered by post edit: {msg}")
-        sms.send_message(msg)
-        return msg
-    return JSONResponse(
-        f"Author is {author_name}, carry on.", 204, {"content-type:": "text/plain"}
-    )
-
-
-@router.post("/post/update")
-async def author_post_updated(post_update: PostUpdate):
-    """
-    Notify admin user when another author modifies their post.
+    Notify admin when new authors create their first post, or edit an admin's post.
 
     :param post_update: Post object generated upon update.
     :param post_update: PostUpdate
@@ -44,11 +22,17 @@ async def author_post_updated(post_update: PostUpdate):
     author_name = data.primary_author.name
     primary_author_id = data.primary_author.id
     authors = data.authors
-    if primary_author_id == "1" and len(authors) > 1:
-        msg = f"{author_name} just updated one of your posts: `{title}`."
-        LOGGER.info(f"SMS notification triggered by post edit: {msg}")
+    if primary_author_id not in ("1", "5dc42cb612c9ce0d63f5bf39"):
+        msg = f"{author_name} just created a post: `{title}`."
+        LOGGER.info(f"SMS triggered by post edit: {msg}")
         sms.send_message(msg)
         return msg
-    return JSONResponse(
-        f"Author is {author_name}, carry on.", 204, {"content-type:": "text/plain"}
-    )
+    elif primary_author_id == "1" and len(authors) > 1:
+        msg = f"{author_name} just updated one of your posts: `{title}`."
+        LOGGER.info(f"SMS triggered by post edit: {msg}")
+        sms.send_message(msg)
+        return msg
+    else:
+        return JSONResponse(
+            f"Author is {author_name}, carry on.", 204, {"content-type:": "text/plain"}
+        )
