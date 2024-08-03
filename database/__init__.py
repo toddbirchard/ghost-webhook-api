f38376f8554c5e2c2dc6d5cca3ade3ce8ce4eedc
Todd Birchard <toddbirchard@gmail.com>
2024-08-02 23:25:33 -0400

    Poetry config.

--- database/__init__.py
+++ database/__init__.py
@@ -1,8 +1,7 @@
 """Initialize custom Database clients for direct read/write access."""
 
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 from config import Settings, settings
 
