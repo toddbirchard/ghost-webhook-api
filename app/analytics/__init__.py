cd8cedc54e62ca0b054e132e61c43dc02d3c4c42
Todd Birchard <toddbirchard@gmail.com>
2025-06-12 17:07:50 -0400

    Remove Algoia-related logic.

--- app/analytics/__init__.py
+++ app/analytics/__init__.py
@@ -33,37 +33,3 @@ async def migrate_site_analytics():
             "rows": monthly_traffic,
         },
     }
-
-
-'''@router.get(
-    "/searches/",
-    summary="Import user search queries.",
-    description="Store user search queries to a SQL database for analysis and suggestive search.",
-    status_code=200,
-)
-async def save_user_search_queries() -> JSONResponse:
-    """
-    Save top search analytics for the current week.
-
-    :returns: JSONResponse
-    """
-    weekly_searches = persist_algolia_searches(settings.ALGOLIA_TABLE_WEEKLY, "7d")
-    monthly_searches = persist_algolia_searches(settings.ALGOLIA_TABLE_MONTHLY, "month")
-    if weekly_searches is None or monthly_searches is None:
-        HTTPException(500, "Unexpected error when saving search query data.")
-    LOGGER.success(
-        f"Inserted {len(weekly_searches)} rows into `{settings.ALGOLIA_TABLE_WEEKLY}`, \
-            {len(monthly_searches)} into `{settings.ALGOLIA_TABLE_MONTHLY}`"
-    )
-    return JSONResponse(
-        {
-            "7-Day": {
-                "count": len(weekly_searches),
-                "rows": weekly_searches,
-            },
-            "90-Day": {
-                "count": len(monthly_searches),
-                "rows": monthly_searches,
-            },
-        }
-    )'''
